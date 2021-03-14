b95649470f66f8494b78436d5fce7ffd385006f9
Ludovic <ludo770213@hotmail.fr>
2021-03-14 16:58:35 +0100

    début templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,11 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template('index.html')
+
+@app.route('/import')
+def importPage():
+    return render_template('import.html')
